8b1e25df598fe46f28f8ba4c636332b5de8e23cb
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-07-15 09:55:47 -0300

    ajuste de configuracao inicial

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true,//adiciona ponto e visrgula no final 
+    trailingComma: 'all',//colocar virgula no final
+    singleQuote: true,//usar sempre aspas simples ao inves de aspas dupla
+    printWidth: 100, //tamanho que o código ocupa
+    tabWidth: 2,//tamanho do espaçamento em tab
+  };
