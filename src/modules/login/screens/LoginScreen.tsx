5fae6ec162081f031732d1fea2d4154eb75233b6
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-07-22 09:02:33 -0300

    adicionado botao do login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -4,8 +4,10 @@ import {
   ContainerLoginScreen,
   LimitedContainer,
   LogoImage,
+  TitleLogin,
 } from '../styles/loginScreen.styles';
 import Input from '../../../shared/inputs/input/input';
+import Button from '../../../shared/buttons/button/Button';
 
 const LoginScreen = () => {
   return (
@@ -13,8 +15,14 @@ const LoginScreen = () => {
     <ContainerLogin>
       <LimitedContainer>
         <LogoImage src="./logo.png" />
+        <TitleLogin level={2} type="secondary">
+            LOGIN
+          </TitleLogin>
         <Input title="USUÁRIO" />
           <Input title="SENHA" />
+          <Button type="primary" margin="64px 0px 16px 0px">
+            ENTRAR
+          </Button>
       </LimitedContainer>
     </ContainerLogin>
     <BackgroundImage src="./background.png"/>
