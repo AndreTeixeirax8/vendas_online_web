d99d05ffedbca0164fccfd325fcffdd0eb6ddd48
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-07-27 20:26:57 -0300

    inserindo notificacoes

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,15 @@
+import './main.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './main.css';
-import type { Router as RemixRouter } from '@remix-run/router';
-import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
+
+import App from './App';
 import { GlobalProvider } from './shared/hooks/useGlobalContext';
-
-import { loginRoutes } from './modules/login/routes';
-
-const mainRoutes: RouteObject[] = [
-  {
-    path: '/',
-    element: <div>Tela Principal</div>,
-    errorElement: <div>Página não encontrada</div>,
-  },
-];
-
-const router: RemixRouter = createBrowserRouter([...mainRoutes, ...loginRoutes]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-     <GlobalProvider>
-      <RouterProvider router={router} />
+    <GlobalProvider>
+      <App />
     </GlobalProvider>
   </React.StrictMode>,
 );
